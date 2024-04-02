e5029f2f77fe49ca21793da57534697093650178
sarathnakka <sarathsai321@gmail.com>
2024-04-02 20:15:21 +0530

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -140,7 +140,7 @@ export const experiences = [
   {
     id: 0,
     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
-    role: "DevOps & Fullstack Engineering Intern",
+    role: "",
     company: "Flipr Inovations Pvt. Ltd.",
     date: "Aug 2023 - July 2023",
     desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
